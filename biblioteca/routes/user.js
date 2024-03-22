f60c7f3ea58221e6e959a6a3340c3a3eb1fc7e3c
fabian rua <fyrua@unicesar.edu.co>
2024-03-22 01:24:09 -0500

    creacion de rutas falta validar

---
+++ biblioteca/routes/user.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router =express.Router();
+const {Employees, validator, User}=require('../models/user');
+const validate =require('../middleware/validate');
+const isValidObjectId=require('../middleware/isValidObjectId')
+const asyncHandler=require('../middleware/asyncHandler')
+
+router.post("/", validate(validator),
+    asyncHandler(async(req,res)=>{
+        await User(req.body).save();
+        res.status(200).send("user created sucessfully")
+    })
+);
+
+router.get(
+    "/",
+    asyncHandler(async(req,res)=>{
+        const users =await User.find();
+        res.send(users)
+    })
+)
+
+router.get(
+    "/:id",
+    isValidObjectId(async(req,res)=>{
+        const users =await User.findById(req.params.id);
+        res.send(users)
+    })
+)
+
+
+router.put(
+    "/",
+    [isValidObjectId, validate(validator)],
+    asyncHandler(async(req,res)=>{
+         await User.findByIdAndUpdate({_id:req.params.id}, req.body)
+         res.status(200).send("user update sucessfully")
+        
+    })
+)
+
+router.delete(
+    "/",
+    [isValidObjectId, validate(validator)],
+    asyncHandler(async(req,res)=>{
+         await User.findByIdAndDelete({_id:req.params.id}, req.body)
+         res.status(200).send("users deleted sucessfully")
+        
+    })
+)
+module.exports=router;
