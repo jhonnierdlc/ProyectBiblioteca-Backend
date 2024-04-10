921dff0ba0543aea52c25455338c118fca1e6e88
JesusDHernandezR <jesusdavidhernandez@unicesar.edu.co>
2024-04-10 01:13:11 -0500

    "terminado crud de libros"

--- biblioteca/routes/libro.js
+++ biblioteca/routes/libro.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const {ModeloLibro, validator } = require('../models/libro');
 const validate = require('../middleware/validate');
 const asyncHandler = require("../middleware/asyncHandler");
+const isValidObjectId = require('../middleware/IsValidObjectId');
 
 router.post("/",
  validate(validator), 
@@ -17,6 +18,26 @@ asyncHandler(async(req,res) => {
     const libro = await ModeloLibro.find();
     res.send(libro)
 })
+)
+
+//Editar
+router.put(
+    "/:id",
+    [isValidObjectId, validate(validator)],
+    asyncHandler(async (req,res) =>{
+        await ModeloLibro.findByIdAndUpdate({_id:req.params.id},req.body);
+        res.status(200).send("Libro Editado Correctamente")
+    })
+)
+
+//Eliminar
+router.delete(
+    "/:id",
+    isValidObjectId,
+    asyncHandler(async (req,res) => {
+        await ModeloLibro.findByIdAndDelete(req.params.id);
+        res.status(200).send("Libro Eliminado Correctamente")
+    })
 )
 
 module.exports = router;
